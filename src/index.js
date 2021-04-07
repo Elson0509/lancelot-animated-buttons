5c10bb6fd095791dce23880e14ac63bb95688a24
Elson Ramos <elson.0509@gmail.com>
2021-04-07 11:10:02 -0400

    exporting LancelotEllipsisButton and LancelotHamburguerButton

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React, {} from 'react'
 import LancelotEllipsisButton from './components/AnimatedEllipsisButton/LancelotEllipsisButton'
 import LancelotHamburguerButton from './components/AnimatedHamburguerButton/LancelotHamburguerButton'
+export { default as LancelotEllipsisButton } from './components/AnimatedEllipsisButton/LancelotEllipsisButton'
+export { default as LancelotHamburguerButton } from './components/AnimatedHamburguerButton/LancelotHamburguerButton'
 
 export const ExampleComponent = ({ text }) => {
   return (
-    <div style={{background:'black', padding:'80px'}}>
-      <LancelotHamburguerButton
-        animationTime={2}
-        background="#ccc"
-        buttonSize={8}
-        clicked={()=>console.log("Lancelot!")}
-        crossColor="#238a0e"
-        disableHoverEffect
-        hamburguerColor="purple"
-        rotationIntensity={10}
-      />
-    </div>
+    <div></div>
   )
 }
